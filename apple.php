096a3c2a9874f78826b6bfee51ef3abdc5c626e8
mungboihzomi <mungpii131@gmail.com>
2024-12-28 22:17:49 +0630

    Create apple.php
    Apple.php
    
    Signed-off-by: mungboihzomi <mungpii131@gmail.com>

---
+++ apple.php
@@ -0,0 +1,102 @@
+[
+  {
+    "description": "JUL-672-SUB [English Subtitle] My Husband Cannot Satisfy Me...The Married Woman Who Fell For The Well-Endowed Shop Manager At Her Part-Time Job. Nao Jinguji.\nJUL00672 夫では満足できなくて…パート先の巨根店長に堕ちた人妻 神宮寺ナオ",
+    "sources": "https://streamtape.net/get_video?id=YZDo8lGKoqI6JB&expires=1735469263&ip=F0IOKRWUEy9XKxR&token=XJyxhRseMwI6&stream=1",
+    "subtitle": "JUL-672",
+    "thumb": "https://cdn002.imggle.net/webp/poster/jul-672.webp",
+    "title": "Nao Jinguji"
+  },
+  {
+    "description": "MDON-011-RM [Reducing Mosaic] Only Available Online: Madonna's Exclusive Starlets Unfettered For Real. Welcome to MADOOOON!!!! POV Fun With Nao Jinguji\nMDON00011 配信限定 マドンナ専属女優の『リアル』解禁。 MADOOOON！！！！ 神宮寺ナオ ハメ撮り",
+    "sources": "https://streamtape.net/get_video?id=ZkpzkMg01aUoAQ&expires=1735469896&ip=F0IOKRWUEy9XKxR&token=AaP7O4DYQkzk&stream=1",
+    "subtitle": "MDON-011",
+    "thumb": "https://cdn001.imggle.net/webp/poster/20231912/1703041021.webp",
+    "title": "Nao Jinguji"
+  },
+  {
+    "description": "JUL-920-SUB [English Subtitle] I Was Raised By My Beloved Stepsister, And She Was Stolen Away By My Worst Friend...Nao Jinguji\nJUL00920 僕を女手一つで育ててくれた、最愛の義姉が最低な友人に寝取られて… 神宮寺ナオ",
+    "sources": "https://streamtape.net/get_video?id=ZxjP0O8bO3Uq9lW&expires=1735470104&ip=F0IOKRWUEy9XKxR&token=TjVfHON-5BZG&stream=1",
+    "subtitle": "JUL-920",
+    "thumb": "https://cdn001.imggle.net/webp/poster/20220707/1657172975.webp",
+    "title": "Nao Jinguji"
+  },
+  {
+    "description": "IPX-183-RM [Reducing Mosaic] About Nine Years Industry History!Legend Actress · Tianwai Tsubasa Taught Nuke Can Remember Absolute Cum SEX Lecture\nIPX183 業界歴約9年！レジェンド女優・天海つばさが教えるヌケて覚えられる絶対絶頂SEX講座.",
+    "sources": "https://dw120l.cloudatacdn.com/u5kj7k5avdc3sdgge5wpwiyfkawutnjtkudslnll577x7fu6rclhuzkcpfra/ayqayp7v5v~4IFlXzZrAG?token=8jdan4mry7rxjc93nuzd2t9l&expiry=1735400666468",
+    "subtitle": "IPX-183",
+    "thumb": "https://cdn001.imggle.net/webp/poster/20242012/1735351406.webp",
+    "title": "Amami Tsubasa"
+  },
+  {
+    "description": "Introducing Chromecast. The easiest way to enjoy online video and music on your TV. For $35. Find out more at google.com/chromecast.",
+    "sources": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4",
+    "subtitle": "By Google",
+    "thumb": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/ForBiggerFun.jpg",
+    "title": "Bigger"
+  },
+  {
+    "description": "Introducing Chromecast. The easiest way to enjoy online video and music on your TV—for the times that call for bigger joyrides. For $35. Learn how to use Chromecast with YouTube and more at google.com/chromecast.",
+    "sources": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+    "subtitle": "By Google",
+    "thumb": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/ForBiggerJoyrides.jpg",
+    "title": "Joyrides"
+  },
+  {
+    "description": "Introducing Chromecast. The easiest way to enjoy online video and music on your TV—for when you want to make Buster's big meltdowns even bigger. For $35. Learn how to use Chromecast with Netflix and more at google.com/chromecast.",
+    "sources": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4",
+    "subtitle": "By Google",
+    "thumb": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/ForBiggerMeltdowns.jpg",
+    "title": "Meltdowns"
+  },
+  {
+    "description": "Sintel is an independently produced short film, initiated by the Blender Foundation as a means to further improve and validate the free/open source 3D creation suite Blender. With initial funding provided by 1000s of donations via the internet community, it has again proven to be a viable development model for both open 3D technology as for independent animation film.\nThis 15 minute film has been realized in the studio of the Amsterdam Blender Institute, by an international team of artists and developers. In addition to that, several crucial technical and creative targets have been realized online, by developers and artists and teams all over the world.\nwww.sintel.org",
+    "sources": [
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4"
+    ],
+    "subtitle": "By Blender Foundation",
+    "thumb": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/Sintel.jpg",
+    "title": "Sintel"
+  },
+  {
+    "description": "Smoking Tire takes the all-new Subaru Outback to the highest point we can find in hopes our customer-appreciation Balloon Launch will get some free T-shirts into the hands of our viewers.",
+    "sources": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4",
+    "subtitle": "By Garage419",
+    "thumb": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/SubaruOutbackOnStreetAndDirt.jpg",
+    "title": "Outback"
+  },
+  {
+    "description": "Tears of Steel was realized with crowd-funding by users of the open source 3D creation tool Blender. Target was to improve and test a complete open and free pipeline for visual effects in film - and to make a compelling sci-fi film in Amsterdam, the Netherlands. The film itself, and all raw material used for making it, have been released under the Creatieve Commons 3.0 Attribution license. Visit the tearsofsteel.org website to find out more about this, or to purchase the 4-DVD box with a lot of extras. (CC) Blender Foundation - http://www.tearsofsteel.org",
+    "sources": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4",
+    "subtitle": "By Blender Foundation",
+    "thumb": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/TearsOfSteel.jpg",
+    "title": "Steel"
+  },
+  {
+    "description": "The Smoking Tire heads out to Adams Motorsports Park in Riverside, CA to test the most requested car of 2010, the Volkswagen GTI. Will it beat the Mazdaspeed3's standard-setting lap time? Watch and see...",
+    "sources": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/VolkswagenGTIReview.mp4",
+    "subtitle": "By Garage419",
+    "thumb": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/VolkswagenGTIReview.jpg",
+    "title": "Volkswagen"
+  },
+  {
+    "description": "The Smoking Tire is going on the 2010 Bullrun Live Rally in a 2011 Shelby GT500, and posting a video from the road every single day! The only place to watch them is by subscribing to The Smoking Tire or watching at BlackMagicShine.com",
+    "sources": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4",
+    "subtitle": "By Garage419",
+    "thumb": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/WeAreGoingOnBullrun.jpg",
+    "title": "Bullrun"
+  },
+  {
+    "description": "The Smoking Tire meets up with Chris and Jorge from CarsForAGrand.com to see just how far $1,000 can go when looking for a car.The Smoking Tire meets up with Chris and Jorge from CarsForAGrand.com to see just how far $1,000 can go when looking for a car.",
+    "sources": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WhatCarCanYouGetForAGrand.mp4",
+    "subtitle": "By Garage419",
+    "thumb": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/WhatCarCanYouGetForAGrand.jpg",
+    "title": "grand"
+  },
+  {
+    "description": "The Smoking Tire meets up with Chris and Jorge from CarsForAGrand.com to see just how far $1,000 can go when looking for a car.The Smoking Tire meets up with Chris and Jorge from CarsForAGrand.com to see just how far $1,000 can go when looking for a car.",
+    "sources": "https://d500.userrive.org/d/3wj3wucr2as5ln23s3l7fsdrgz5yejdorkuu5zd5oyes6pybjths4fehhzux2hskhn7nd4ia/Extraordinary%20Rescue%20(2023)%201080p%20WEB-DL%20AAC2.0-CM.mkv",
+    "subtitle": "By Garage",
+    "thumb": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/WhatCarCanYouGetForAGrand.jpg",
+    "title": "grand"
+  }
+]
