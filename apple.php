77d04b5c84f95e7a6c8c5d74215bead269ac4bf6
mungboihzomi <mungpii131@gmail.com>
2024-12-29 00:34:39 +0630

    Update apple.php
    Apple
    
    Signed-off-by: mungboihzomi <mungpii131@gmail.com>

--- apple.php
+++ apple.php
@@ -1,1 +1,11 @@
-dddd
+{"categories":[
+{"name":"Recent", "image":"https://raw.githubusercontent.com/mungboihzomi/mungboihzomi/refs/heads/main/Picsart_24-11-08_19-04-15-263.jpg"
+},
+{"name":"Action", "image":"https://raw.githubusercontent.com/mungboihzomi/mungboihzomi/refs/heads/main/Picsart_24-11-08_19-04-15-263.jpg"
+},
+{"name":"Drama", "image":"https://raw.githubusercontent.com/mungboihzomi/mungboihzomi/refs/heads/main/Picsart_24-11-08_19-04-15-263.jpg"
+},
+{"name":"Horror", "image":"https://raw.githubusercontent.com/mungboihzomi/mungboihzomi/refs/heads/main/Picsart_24-11-08_19-04-15-263.jpg"
+},
+{"name":"Suspense", "image":"https://raw.githubusercontent.com/mungboihzomi/mungboihzomi/refs/heads/main/Picsart_24-11-08_19-04-15-263.jpg"
+}]}
